daf5866149080a90c08b9b3432daad802ca46165
vanquyettran <vanquyettran@hotmail.com>
2023-01-29 19:51:11 +0700

    Add polyfill for finally

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,2 +1,17 @@
 import 'mdn-polyfills/Array.prototype.includes';
 import 'mdn-polyfills/String.prototype.startsWith';
+
+if (Promise.prototype.finally === undefined) {
+    Promise.prototype.finally = function (callback) {
+        if (typeof callback !== 'function') {
+            return this.then(callback, callback);
+        }
+        const P = this.constructor || Promise;
+        return this.then(
+            value => P.resolve(callback()).then(() => value),
+            error => P.resolve(callback()).then(() => {
+                throw error;
+            })
+        );
+    };
+}
