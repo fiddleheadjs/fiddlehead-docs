0ff9cb2ed987e6e27fb9a65c68e1638830738d0b
quyettvq <quyettvq@gmail.com>
2024-06-16 02:17:05 +0700

    cotuongpc1: update css

--- src/co-tuong-pc1/avatar/Avatar.js
+++ src/co-tuong-pc1/avatar/Avatar.js
@@ -3,7 +3,7 @@ import './Avatar.less';
 export let Avatar = ({player}) => {
     return (
         <div class="Avatar">
-            <img src={`/img/cotuongpc1/avatars/${player.id}.jpg`} style={{visibility: 'hidden'}} onload="this.style.visibility='visible';" />
+            <div class="background-image" style={{ backgroundImage: `url(/img/cotuongpc1/avatars/${player.id}.jpg)` }} />
         </div>
     );
 };
