1750eb5847e47a656bd58155455560c8f88de5e8
quyettvq <quyettvq@gmail.com>
2024-07-06 10:50:35 +0700

    cotuongpc1: update avatars

--- src/cotuongpc1/avatar/Avatar.js
+++ src/cotuongpc1/avatar/Avatar.js
@@ -3,7 +3,7 @@ import './Avatar.less';
 export let Avatar = ({player}) => {
     return (
         <div class="Avatar">
-            <div class="background-image" style={{ backgroundImage: `url(/img/cotuongpc1/avatars/${player.id}.jpg?v=2)` }} />
+            <div class="background-image" style={{ backgroundImage: `url(/img/cotuongpc1/avatars/${player.id}.jpg?v=3)` }} />
         </div>
     );
 };
