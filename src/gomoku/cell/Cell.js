abf46d587e1c8682f617a354b92e5816ffd6d658
Tran Van Quyet <quyettvq@gmail.com>
2025-02-18 12:46:17 +0700

    gomoku update style

--- src/gomoku/cell/Cell.js
+++ src/gomoku/cell/Cell.js
@@ -1,28 +1,38 @@
 import './Cell.less';
 
 export let Cell = ({ value, rx, cx, setMatrix, teamId, userId, tableCode, streak, setMoved, locked, setGameData }) => {
-    let viewOnly = locked || streak.length > 0 || teamId == null || userId == null || tableCode == null || setGameData == null;
+    let viewOnly = (
+        locked ||
+        streak.length > 0 ||
+        teamId == null ||
+        userId == null ||
+        tableCode == null ||
+        setGameData == null
+    );
     
+    let move = () => {
+        if (viewOnly || value !== 2) {
+            return;
+        }
+        setMoved(true);
+        setMatrix(matrix => {
+            let row = [...matrix[rx]];
+            row[cx] = teamId;
+            matrix[rx] = row;
+            return [...matrix];
+        });
+        fetch(`/gomoku/move?row=${rx}&cell=${cx}&userId=${userId}&tableCode=${tableCode}`).then(res => res.json()).then(data => {
+            setGameData(data);
+        });
+    };
+
     return (
         <td
             class={`Cell ${viewOnly ? 'view-only' : ''}`}
             data-value={value}
             data-streak={streak.includes(`${rx}:${cx}`)}
-            onClick={() => {
-                if (viewOnly || value !== 2) {
-                    return;
-                }
-                setMoved(true);
-                setMatrix(matrix => {
-                    let row = [...matrix[rx]];
-                    row[cx] = teamId;
-                    matrix[rx] = row;
-                    return [...matrix];
-                });
-                fetch(`/gomoku/move?row=${rx}&cell=${cx}&userId=${userId}&tableCode=${tableCode}`).then(res => res.json()).then(data => {
-                    setGameData(data);
-                });
-            }}
+            data-myteam={teamId}
+            onClick={() => move()}
         />
     );
 };
