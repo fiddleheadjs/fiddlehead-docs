004140775ae33ddf0d958f4c123bf98be1868c48
quyettvq <quyettvq@gmail.com>
2025-12-06 20:05:29 +0700

    banner section

--- src/aimba/AiMba.js
+++ src/aimba/AiMba.js
@@ -1,10 +1,12 @@
 import './AiMba.less';
+import {Banner} from './banner/Banner';
 import {TopBar} from './top-bar/TopBar';
 
 export let AiMba = () => {
     return (
         <div class="AiMba">
             <TopBar />
+            <Banner />
         </div>
     );
 };
---
+++ src/aimba/banner/Banner.js
@@ -0,0 +1,50 @@
+import {SquareCheck} from '../icons/SquareCheck';
+import './Banner.less';
+
+export let Banner = () => {
+    return (
+        <div class="Banner">
+            <div class="container">
+                <div class="overlay">
+                    <div class="group">
+                        <div class="headline">
+                            <div class="brand">
+                                <span>AiMBA</span>
+                            </div>
+                            <div class="slogan">
+                                Agile & Immersive MBA-level training
+                            </div>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <div class="description">Học MBA thực chiến với AI qua mô phỏng hàng nghìn tình huống thật.</div>
+                    </div>
+                    <div class="group">
+                        <ul class="checklist">
+                            <li>
+                                <SquareCheck />
+                                <span>Cung cấp hệ thống kiến thức</span>
+                            </li>
+                            <li>
+                                <SquareCheck />
+                                <span>Mài sắc tư duy quản lý</span>
+                            </li>
+                            <li>
+                                <SquareCheck />
+                                <span>Kỹ năng và công cụ quản trị</span>
+                            </li>
+                            <li>
+                                <SquareCheck />
+                                <span>Trợ lý AI giải quyết nhanh công việc</span>
+                            </li>
+                            <li>
+                                <SquareCheck />
+                                <span>Năng lực quản trị chuẩn quốc tế</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
