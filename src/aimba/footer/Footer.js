6cf73c709969130a065a40f9da6190fcdfe99b2f
Tran Van Quyet <vanquyettran@hotmail.com>
2025-12-14 06:08:52 +0700

    Footer

--- src/aimba/footer/Footer.js
+++ src/aimba/footer/Footer.js
@@ -5,6 +5,17 @@ import './Footer.less';
 export let Footer = () => {
     return (
         <footer class="Footer">
+            <div class="soft-edge">
+                <svg viewBox="0 0 1920 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M396.468 48.7473C184.485 52.9502 41.9966 93.9364 -1 118.471V370.764L1921 371C1920.5 277.892 1921.2 73.7821 1920.2 0C1878.7 27.6989 1725.86 55.7314 1554.88 73.0847C1336.89 94.7764 1028.42 81.094 924.426 73.0847C590.453 50.3917 446.964 47.7462 396.468 48.7473Z" fill="url(#soft-edge-linear-gradient)" />
+                    <defs>
+                        <linearGradient id="soft-edge-linear-gradient" x1="0" y1="150" x2="1920" y2="150" gradientUnits="userSpaceOnUse">
+                            <stop offset="0.25" stop-color="#1E469E" />
+                            <stop offset="1" stop-color="#28A7DF" />
+                        </linearGradient>
+                    </defs>
+                </svg>
+            </div>
             <div class="main-area">
                 <div class="container">
                     <div class="about-us">
