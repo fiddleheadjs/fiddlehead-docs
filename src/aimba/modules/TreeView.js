7a3827d78860775f232c4caa1e245cd89b24b098
Tran Van Quyet <vanquyettran@hotmail.com>
2025-12-24 03:28:56 +0700

    AiMBA: TreeView improvement and bugfix

--- src/aimba/modules/TreeView.js
+++ src/aimba/modules/TreeView.js
@@ -1,37 +1,40 @@
 import './TreeView.less';
-import {useState} from 'fiddlehead';
+import {useRef, useState} from 'fiddlehead';
 import {TriangleDown, TriangleUp} from '../icons';
 import {TreeViewCard} from './TreeViewCard';
 
-let isSingleColumnOrUnknown = () => {
-    return globalThis.innerWidth == null || globalThis.innerWidth <= 600;
-};
-
-let isTwinColumnOrUnknown = () => {
-    return globalThis.innerWidth == null || globalThis.innerWidth > 600;
+let isTwinColumnOrUnknownApproximately = () => {
+    return window.innerWidth == null || window.innerWidth > 600;
 };
 
 export let TreeView = ({data}) => {
     let [variableExpandedIndexes, setVariableExpandedIndexes] = useState(() => {
-        return isTwinColumnOrUnknown() ? [0, 1] : [0];
+        return isTwinColumnOrUnknownApproximately() ? [0, 1] : [0];
     });
     let fixedExpandedIndexes = [];
     let expandedIndexes = [...fixedExpandedIndexes, ...variableExpandedIndexes];
     
+    let cardsRef = useRef(null);
+    let isSingleColumnOrUnknown = () => {
+        let grid = cardsRef.current;
+        if (grid == null) {
+            return true;
+        }
+        let {gridTemplateColumns} = getComputedStyle(grid);
+        return !gridTemplateColumns.includes(' ');
+    };
+
     let toggleExpanded = (index) => {
         if (fixedExpandedIndexes.includes(index)) {
             return;
         }
-
         let targetIndexes = isSingleColumnOrUnknown() ? [index] : (
             index % 2 === 0 ? [index, index + 1] : [index - 1, index]
         );
-
         setVariableExpandedIndexes((prevValue) => {
             if (variableExpandedIndexes.includes(index)) {
                 return prevValue.filter(idx => !targetIndexes.includes(idx));
             }
-
             return targetIndexes;
         });
     };
@@ -43,7 +46,7 @@ export let TreeView = ({data}) => {
                 <div class="line" />
                 <TriangleDown />
             </div>
-            <div class="cards">
+            <div class="cards" ref={cardsRef}>
                 {data.map(([title, description], index) => (
                     <TreeViewCard
                         key={title}
