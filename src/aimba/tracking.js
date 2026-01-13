e9121f3d7b180c196930f235bdbac0a1716854e7
quyettvq <quyettvq@gmail.com>
2026-01-13 09:48:47 +0700

    update tracking

--- src/aimba/tracking.js
+++ src/aimba/tracking.js
@@ -8,7 +8,7 @@ export let trackLeadEvent = () => {
 
 export let trackSubscribeEventIfAny = () => {
     // Track payment success notification page
-    // https://aimba.vn/payment-success?payment_success=true&orderCode=3532837057757&package=Pro%20730&amount=111
+    // https://aimba.vn/payment-success?payment_success=true&orderCode=3532837057757&package=Pro%20730&amount=600000
     try {
         let url = new URL(location.href);
         if (url.pathname !== '/payment-success') {
@@ -18,6 +18,7 @@ export let trackSubscribeEventIfAny = () => {
         if (amount === null) {
             return;
         }
+        amount = amount.replace(/\D/g, '');
         let flag = sessionStorage.getItem('fbq.track.subscribe');
         if (flag === amount) {
             return;
